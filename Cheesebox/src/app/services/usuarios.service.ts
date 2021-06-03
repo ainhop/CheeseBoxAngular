b3acf181227ddb37d06fd45ec453b7a17e8c5f57
Ainhoa PG <ainhoapalacio>
2021-06-03 23:10:30 +0200

    formulario-registro: ts & html

--- Cheesebox/src/app/services/usuarios.service.ts
+++ Cheesebox/src/app/services/usuarios.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuariosService {
+  private baseUrl: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = '';
+  }
+  registro(formValues: any) {
+    return this.httpClient
+      .post(`${this.baseUrl}/registro`, formValues)
+      .toPromise();
+  }
 }
