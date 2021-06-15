bb1bbf8f430d21a6592fba4aa5923e415d8b01f9
ainhoa <ainhoapalacio>
2021-06-15 14:05:07 +0200

    recetas

--- Cheesebox/src/app/services/recetas.service.ts
+++ Cheesebox/src/app/services/recetas.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Receta } from '../interfaces/recetas.interfaces'
+import { Receta } from '../interfaces/recetas.interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecetasService {
-
   private baseUrl: string;
 
   constructor(private httpClient: HttpClient) {
-    this.baseUrl = 'http://localhost:3000/';
-  }
-  create(pReceta: Receta) {
-    return this.httpClient.post(this.baseUrl, pReceta).toPromise();
+    this.baseUrl = 'http://localhost:3000/recetas';
   }
 
+  getAll(): Promise<Receta[]> {
+    return this.httpClient.get<Receta[]>(`${this.baseUrl}`).toPromise();
+  }
+
+  create(pReceta: Receta) {
+    return this.httpClient.post(`${this.baseUrl}`, pReceta).toPromise();
+  }
 }
