deba54758510bb6a726f3cf783d55fb52724dbb8
ainhoa <ainhoapalacio>
2021-06-25 17:53:14 +0200

    la chorrada padre de cambiar una l por una L. así somos los genios.

--- Cheesebox/src/app/app.component.ts
+++ Cheesebox/src/app/app.component.ts
@@ -1,40 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, HostListener } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { UsuariosService } from './services/usuarios.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'Cheesebox';
- 
-  constructor(private router: Router, public usuariosService: UsuariosService, httpComponente: HttpClient ) { }
-  
-  @HostListener("window:scroll", []) onWindowScroll() {
+
+  constructor(
+    private router: Router,
+    public usuariosService: UsuariosService
+  ) {}
+
+  @HostListener('window:scroll', []) onWindowScroll() {
     this.scrollFunction();
   }
 
-scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
+  scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      document.getElementById('myBtn').style.display = 'block';
     } else {
-        document.getElementById("myBtn").style.display = "none";
+      document.getElementById('myBtn').style.display = 'none';
     }
-}
-
-
-topFunction() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
   }
-  
+
+  topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
   onLogout() {
-
     localStorage.removeItem('token');
-
-    }
+  }
 }
-
